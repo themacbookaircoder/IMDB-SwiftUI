f91adb7951d852d96fe77dd9d5bbac1ec64ad7ed
kuldeep <kuldipaavashist@gmail.com>
2022-02-22 07:22:00 +0530

    Initial Commit

---
+++ IMDB-SwiftUI/IMDB_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDB_SwiftUIApp.swift
+//  IMDB-SwiftUI
+//
+//  Created by Kuldeep Vashisht on 22/02/22.
+//
+
+import SwiftUI
+
+@main
+struct IMDB_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
